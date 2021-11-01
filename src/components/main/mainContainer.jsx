89f488f1ec3b7c2cd9c575142fd0ec63388aa0e1
a-shirokov-exceed <ashirokov.exceedteam@gmail.com>
2021-11-01 18:28:18 +0300

    Sidebar with links

--- src/components/main/mainContainer.jsx
+++ src/components/main/mainContainer.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { Container, Typography } from '@material-ui/core';
+import SideBar from '../sidebar/sideBar';
 import './mainContainer.scss';
 
 const MainContainer = () => {
   return (
-    <Container className="main-container">      
-      <Typography className="sidebar" variant='h1'>
-        sidebar this
-      </Typography>
+    <Container className="main-container">
+      <SideBar />
       <Typography className="content" variant='h1'>
         content this
       </Typography>
--- src/components/sidebar/sideBar.jsx
+++ src/components/sidebar/sideBar.jsx
@@ -1,1 +1,15 @@
+import { Switch, Route, Redirect, Link } from 'react-router-dom';
+import { Container } from '@material-ui/core';
 import './sideBar.scss';
+
+const SideBar = () => {
+
+  return (
+    <Container className="sidebar">
+      <Link to="/departments">Departments</Link>
+      <Link to="/employee">Employee</Link>
+    </Container>
+  )
+}
+
+export default SideBar;
